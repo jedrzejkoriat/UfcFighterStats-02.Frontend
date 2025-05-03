f2cb2ca82e49902a96282835cc2b7d4f8ee70480
jedrzejkoriat <jedrzej.koriat@gmail.com>
2025-05-04 01:36:45 +0200

    Add small changes

---
+++ src/components/FighterList.tsx
@@ -0,0 +1,15 @@
+function FighterList() {
+  return (
+    <>
+      <ul className="list-group">
+        <li className="list-group-item">Fighter</li>
+        <li className="list-group-item">Fighter</li>
+        <li className="list-group-item">Fighter</li>
+        <li className="list-group-item">Fighter</li>
+        <li className="list-group-item">Fighter</li>
+      </ul>
+    </>
+  );
+}
+
+export default FighterList;
---
+++ src/components/WeightClassAccordion.tsx
@@ -0,0 +1,35 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import FighterList from "./FighterList";
+
+function WeightClassAccordion() {
+  return (
+    <div className="accordion" id="accordionExample">
+      <div className="accordion-item">
+        <h2 className="accordion-header">
+          <button
+            className="accordion-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseOne"
+            aria-expanded="true"
+            aria-controls="collapseOne"
+          >
+            Accordion Item #1
+          </button>
+        </h2>
+        <div
+          id="collapseOne"
+          className="accordion-collapse collapse show"
+          data-bs-parent="#accordionExample"
+        >
+          <div className="accordion-body">
+            <FighterList />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default WeightClassAccordion;
